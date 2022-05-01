c0f21ce6331921bd150e893f4cd35a1aacf93975
Alexis Manin <alexis.manin@gmail.com>
2022-05-01 19:40:02 +0200

    Practice step 2: done

--- src/main/kotlin/fr/amanin/demos/hellospringboot/HelloSpringBootApplication.kt
+++ src/main/kotlin/fr/amanin/demos/hellospringboot/HelloSpringBootApplication.kt
@@ -2,10 +2,20 @@ package fr.amanin.demos.hellospringboot
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Mono
 
 @SpringBootApplication
 class HelloSpringBootApplication
 
 fun main(args: Array<String>) {
 	runApplication<HelloSpringBootApplication>(*args)
+}
+
+@RestController
+class HelloService {
+
+	@GetMapping("hello")
+	fun hello() = Mono.just("Hello !")
 }
