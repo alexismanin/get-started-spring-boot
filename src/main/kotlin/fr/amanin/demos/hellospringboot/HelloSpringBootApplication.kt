835c92ce9c37e638f4c4033e519aa82f33287e04
Alexis Manin <alexis.manin@gmail.com>
2022-05-01 19:54:56 +0200

    Practice step 3: done

--- src/main/kotlin/fr/amanin/demos/hellospringboot/HelloSpringBootApplication.kt
+++ src/main/kotlin/fr/amanin/demos/hellospringboot/HelloSpringBootApplication.kt
@@ -1,9 +1,15 @@
 package fr.amanin.demos.hellospringboot
 
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Profile
+import org.springframework.stereotype.Component
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.bodyToMono
 import reactor.core.publisher.Mono
 
 @SpringBootApplication
@@ -14,8 +20,24 @@ fun main(args: Array<String>) {
 }
 
 @RestController
+@Profile("server")
 class HelloService {
 
 	@GetMapping("hello")
 	fun hello() = Mono.just("Hello !")
+}
+
+@Component
+@Profile("client")
+class HelloClient(@Value("\${server.port}") val port: Int) : CommandLineRunner {
+	val cli = WebClient.create()
+
+	override fun run(vararg args: String?) {
+		val msg = cli.get().uri("http://localhost:$port/hello")
+			.retrieve()
+			.bodyToMono<String>()
+			.block()
+
+		println(msg)
+	}
 }
